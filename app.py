d8fbbf82fa5ed0547e232eae44467d9826d0db50
FirmMinds-ai <87647533+FirmMinds-ai@users.noreply.github.com>
2021-11-15 13:13:14 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask,request,render_template
-
+import pickle
 
 app = Flask(__name__)
 
+with open('diabetes_std.pkl', 'rb') as file:
+    scaler = pickle.load(file)
 
+with open('diabetes_rfc.pkl', 'rb') as file:
+     rfc = pickle.load(file)
 
 @app.route('/', methods =["GET", "POST"])
 def home():
@@ -16,7 +20,7 @@ def pred():
     if request.method == "POST":
         x1 =  request.form.get('x1')
         x2 =  request.form.get('x2')
-        x3 = request.form.get('x3')
+        x3 =  request.form.get('x3')
         x4 =  request.form.get('x4')
         x5 =  request.form.get('x5')
         x6 =  request.form.get('x6')
@@ -24,8 +28,18 @@ def pred():
         x8 =  request.form.get('x8')
 
         print(x1,x2,x3,x4,x5,x6,x7,x8)
+        l = [float(x1),float(x2),float(x3),float(x4),float(x5),float(x6),float(x7),float(x8)]
+        l = scaler.transform([l])
+        out = rfc.predict(l)
+        print(out)
+        if out[0] == 0:
+            pred = "Normal"
+        else :
+            pred = "Diabetic"
 
-        pred = "Diabetic"
+
+
+
         return render_template("pred.html",pred = pred)
     return render_template("pred.html",pred = False)
 
